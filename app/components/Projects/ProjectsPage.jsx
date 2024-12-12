b88d8f232f21b4ac729febc81bf1dd08f90f50cf
VladimirMedv <vvmedvedev1985@gmail.com>
2024-12-12 13:53:30 -0300

    paddings for projects

--- app/components/Projects/ProjectsPage.jsx
+++ app/components/Projects/ProjectsPage.jsx
@@ -4,7 +4,7 @@ import projects from '../../data/projects'
 
 export default function Projects() {
   return (
-    <section className='bg-primary text-text py-16'>
+    <section className='bg-primary text-text 2lg:py-[12.5rem] sm:py-[9.375rem] max-sm:py-[7.5rem] '>
       <div className=' 2lg:px-10 px-5'>
         {/* Список проектов в столбик */}
         <div className='flex flex-col 2lg:gap-10 gap-5'>

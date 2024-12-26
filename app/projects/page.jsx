d97d9c96541801a3d71098aff6dcb6059f3611e3
VladimirMedv <vvmedvedev1985@gmail.com>
2024-12-26 15:56:09 -0300

    submenu services horizontal

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import { useState } from 'react'
 import ProjectsList from '../components/Projects/ProjectsList'
 import Slogan from '../components/Projects/Slogan'
 
@@ -31,29 +31,38 @@ function PageProjects() {
             <span className='3xl:text-[1.375rem] 3xl:leading-[2.125rem] text-sm leading-[23.87px] font-normal'>
               Choose a Category
             </span>
-            <aside className='3xl:h-[420px] 2lg:h-[295px] font-medium 3xl:text-[2.5rem] 2lg:text-[1.75rem] text-[1.375rem] 3xl:leading-[60px] 2lg:leading-[2.633rem] leading-[2.063rem]'>
-              <ul className='flex 2lg:flex-col flex-row 3xl:gap-1.5 2lg:gap-1 2lg:flex-nowrap flex-wrap'>
-                {categories.map((category, index) => (
-                  <React.Fragment key={index}>
-                    <li
+            <aside className='3xl:h-[420px] 2lg:h-[295px] 2lg:w-fit 7md:w-[688px] 3sm:w-[357px] font-medium 3xl:text-[2.5rem] 2lg:text-[1.75rem] text-[1.375rem] 3xl:leading-[60px] 2lg:leading-[2.633rem] leading-[2.063rem]'>
+              <ul className='flex 2lg:flex-col flex-row 7md:gap-2 3sm:gap-1 2lg:flex-nowrap flex-wrap'>
+                {categories.map((category, index) => {
+                  const isLast = index === categories.length - 1
+
+                  return (
+                    <div
                       key={category}
-                      className={`cursor-pointer text-nowrap ${
-                        activeCategory === category
-                          ? 'text-secondary underline'
-                          : 'hover:text-secondary hover:underline transition-colors duration-300'
-                      }`}
-                      onClick={() => setActiveCategory(category)}
+                      className='flex items-center'
                     >
-                      {category}
-                    </li>
-                    {/* Добавляем запятую только для маленьких экранов и не последнего элемента */}
-                    {index < categories.length - 1 && (
-                      <span className='hidden max-2lg:inline text-text pr-1'>
-                        ,
-                      </span>
-                    )}
-                  </React.Fragment>
-                ))}
+                      <li
+                        className={`cursor-pointer text-nowrap ${
+                          activeCategory === category
+                            ? 'text-secondary underline'
+                            : 'hover:text-secondary hover:underline transition-colors duration-300'
+                        }`}
+                        onClick={() => setActiveCategory(category)}
+                      >
+                        {category}
+                      </li>
+
+                      {!isLast && (
+                        <span
+                          className='hidden max-2lg:inline text-text'
+                          aria-hidden='true'
+                        >
+                          ,
+                        </span>
+                      )}
+                    </div>
+                  )
+                })}
               </ul>
             </aside>
           </div>

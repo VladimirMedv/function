6fd9643466ec94ce350c1a1fa454144a7acf898c
VladimirMedv <vvmedvedev1985@gmail.com>
2025-01-18 22:20:16 -0300

    added gap for main page 3sm

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import AboutMe from './components/AboutMe/AboutMe'
 
 export default function Home() {
   return (
-    <div className='bg-primary text-text flex flex-col 3xl:gap-[12.5rem] 7md:gap-[9.375rem]  4sm:gap-[7.5rem]'>
+    <div className='bg-primary text-text flex flex-col 3xl:gap-[12.5rem] 7md:gap-[9.375rem] 3sm:gap-[7.5rem]'>
       <Hero />
       <Projects />
       <Services sectionStyle='3xl:-mt-[3.75rem] -mt-[2.5rem] 3xl:px-56 2lg:px-40' />
